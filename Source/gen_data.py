9560004638b878643da27e43a66eed0be6080211
Hakkimotorcu07 <hakkimotorcu07@gmail.com>
2022-06-03 13:34:27 +0300

    Update gen_data.py

--- Source/gen_data.py
+++ Source/gen_data.py
@@ -1,7 +1,7 @@
 from lib.data.data_tools.sat2coco_conv import sat2coco
 from lib.data.data_tools.vis2coco_conv import vis2coco
 purpose="train"
-path="../Datasets/Visdrone/train"
+path="../Datasets/WPAFB/train"
 
 #sat2coco(purpose,path)
-vis2coco(purpose,path)
+sat2coco(purpose,path)
